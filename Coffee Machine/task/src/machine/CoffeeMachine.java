25ae4bccff07cca5dcb35b5bc9d3b4e08bfc9291
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2020-06-18 22:24:36 +0200

    Complete Stage #4: Action! Add a coffeeMachine object, give it attributes based on ingredients and some class methods to handle them. (No information hiding yet..)

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,23 +3,83 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
+
+    private int water, milk, coffeeBeans, disposableCups, money;
+
+    public CoffeeMachine() {
+        water = 400;
+        milk = 540;
+        coffeeBeans = 120;
+        disposableCups = 9;
+        money = 550;
+    }
+
+    private void printInventory() {
+        System.out.println(water + " of water");
+        System.out.println(milk + " of milk");
+        System.out.println(coffeeBeans + " of coffee beans");
+        System.out.println(disposableCups + " of disposable cups");
+        System.out.println(money + " of money");
+    }
+
+    private void depleteIngredients(int coffeeType) {
+
+        if (coffeeType == 1) {
+            water -= 250;
+            coffeeBeans -= 16;
+            money += 4;
+        }
+        else if (coffeeType == 2) {
+            water -= 350;
+            milk -= 75;
+            coffeeBeans -= 20;
+            money += 7;
+        }
+        else if (coffeeType == 3) {
+            water -= 200;
+            milk -= 100;
+            coffeeBeans -= 12;
+            money += 6;
+        }
+        disposableCups--;
+        printInventory();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water = scanner.nextInt();
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
 
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = scanner.nextInt();
+        System.out.println("The coffee machine has:");
+        coffeeMachine.printInventory();
 
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffeeBeans = scanner.nextInt();
+        System.out.println("Write action (buy, fill, take):");
+        String choice = scanner.next();
 
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = scanner.nextInt();
-
-        checkInventory(water, milk, coffeeBeans, cups);
-
+        switch (choice) {
+            case "buy":
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                int coffeeType = scanner.nextInt();
+                coffeeMachine.depleteIngredients(coffeeType);
+                break;
+            case "fill":
+                System.out.println("Write how many ml of water do you want to add:");
+                coffeeMachine.water += scanner.nextInt();
+                System.out.println("Write how many ml of milk do you want to add:");
+                coffeeMachine.milk += scanner.nextInt();
+                System.out.println("Write how many grams of coffee beans do you want to add:");
+                coffeeMachine.coffeeBeans += scanner.nextInt();
+                System.out.println("Write how many disposable cups of coffee do you want to add:");
+                coffeeMachine.disposableCups += scanner.nextInt();
+                coffeeMachine.printInventory();
+                break;
+            case "take":
+                System.out.println("I gave you $" + coffeeMachine.money);
+                coffeeMachine.money = 0;
+                coffeeMachine.printInventory();
+            default:
+                System.out.print("There was something wrong with your choice..");
+        }
     }
 
     private static void checkInventory(int water, int milk, int coffeeBeans, int cups) {
