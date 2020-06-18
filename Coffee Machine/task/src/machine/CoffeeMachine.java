e57ed4e622242a36c63a80eaa48630f452dbea97
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2020-06-18 21:18:48 +0200

    Complete Stage #3: Enough coffee for everyone

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,24 +4,48 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
-
-        System.out.println("Write how many cups of coffee you will need:");
         Scanner scanner = new Scanner(System.in);
 
-        int cups;
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = scanner.nextInt();
 
-        while (scanner.hasNext()) {
-            cups = scanner.nextInt();
-            countIngredients(cups);
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeBeans = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = scanner.nextInt();
+
+        checkInventory(water, milk, coffeeBeans, cups);
+
+    }
+
+    private static void checkInventory(int water, int milk, int coffeeBeans, int cups) {
+
+        int amountOfCups = 0;
+
+        do {
+            water -= 200;
+            milk -= 50;
+            coffeeBeans -= 15;
+            ++amountOfCups;
         }
+        while (water >= 200 && milk >= 50 && coffeeBeans >= 15);
 
+        if (amountOfCups == cups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        }
+        else if (amountOfCups > cups) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (amountOfCups - cups) + " more than that)");
+        }
+        else if (amountOfCups == 0) {
+            System.out.println("No, I can make only 0 cup(s) of coffee");
+        }
+        else {
+            System.out.println("No, I can make only " + amountOfCups + " cup(s) of coffee");
+        }
     }
 
     private static void countIngredients(int cups) {
