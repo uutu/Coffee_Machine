f7c852172abe3144db0774b3df21f10df5ca0e1d
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2020-06-23 01:03:42 +0200

    Clean up code, finish Stage#6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -115,40 +115,4 @@ public class CoffeeMachine {
         }
         while (keepOperating);
     }
-
-
-
-    /*private static void checkInventory(int water, int milk, int coffeeBeans, int cups) {
-
-        int amountOfCups = 0;
-
-        do {
-            water -= 200;
-            milk -= 50;
-            coffeeBeans -= 15;
-            ++amountOfCups;
-        }
-        while (water >= 200 && milk >= 50 && coffeeBeans >= 15);
-
-        if (amountOfCups == cups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        }
-        else if (amountOfCups > cups) {
-            System.out.println("Yes, I can make that amount of coffee (and even " + (amountOfCups - cups) + " more than that)");
-        }
-        else if (amountOfCups == 0) {
-            System.out.println("No, I can make only 0 cup(s) of coffee");
-        }
-        else {
-            System.out.println("No, I can make only " + amountOfCups + " cup(s) of coffee");
-        }
-    }*/
-
-    private static void countIngredients(int cups) {
-
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(cups * 200 + " ml of water");
-        System.out.println(cups * 50 + " ml of milk");
-        System.out.println(cups * 15 + " g of coffee beans");
-    }
 }
